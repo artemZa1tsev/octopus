4690002549e32d6d33f53dacf50de574d0203125
artemZa1tsev <78979721+artemZa1tsev@users.noreply.github.com>
2021-02-17 15:02:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.onmousemove = function (event) {
+    let x = event.x - 867;
+    let y = event.y - 290;
+    document.querySelector('.octopus__eye').style.transform = 'rotate(' + 57.2958 * arcctg(x, y) + 'deg)';
+
+    function arcctg(x, y) {
+        if (x > 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+        if (x < 0 && y > 0) return Math.PI / 2 - Math.atan(x / y);
+        if (x < 0 && y < 0) return Math.PI + Math.atan(y / x);
+        if (x > 0 && y < 0) return 3 * Math.PI / 2 + Math.abs(Math.atan(x / y));
+    }
+}
