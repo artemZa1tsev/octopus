4d7bfa739e0191e0bcac6bd9a9164b43b454ed08
artemZa1tsev <78979721+artemZa1tsev@users.noreply.github.com>
2021-02-28 07:02:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.onmousemove = function (event) {
-    let x = event.x - 867;
-    let y = event.y - 290;
+    let x = event.x - 1000;
+    let y = event.y - 380;
     document.querySelector('.octopus__eye').style.transform = 'rotate(' + 57.2958 * arcctg(x, y) + 'deg)';
 
     function arcctg(x, y) {
@@ -10,3 +10,5 @@ document.onmousemove = function (event) {
         if (x > 0 && y < 0) return 3 * Math.PI / 2 + Math.abs(Math.atan(x / y));
     }
 }
+
+    
